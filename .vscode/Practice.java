6154d48a89994844d5e340fc28cfb1d115b2e09e
kcliving <Kevclivingston@gmail.com>
2020-06-15 11:42:44 -0400

    Created a Scanner for input and checked if it is a integer

--- .vscode/Practice.java
+++ .vscode/Practice.java
@@ -22,59 +22,69 @@ public class Practice {
             System.out.println("2. EDIT AN EXISTING COW");
             System.out.println("3. REMOVE AN EXISTING COW");
             System.out.println("4. READ STORED RECORDS");
+            Scanner sc = new Scanner(System.in);//creates a new Scanner for input
+            String input = sc.nextLine();// saves the input
+            Boolean num_check = inputcheck(input);//checks if input is a number
+            System.out.println(num_check);//print result
+            if (num_check == true){//if input is a number
+                int value = Integer.parseInt(input);//convert input to a number
+                System.out.println(value);
+            }
+            System.out.println(input);
+            sc.close();
+
             flag = false;
         }
         while ((line = read.readLine()) != null) {
-            String[] pieces = line.split(" ");//breaks up the string with each space
-            if(pieces.length == 4){// makes sure that the record contains 4 values
+            String[] pieces = line.split(" ");// breaks up the string with each space
+            if (pieces.length == 4) {// makes sure that the record contains 4 values
                 System.out.println("GOOD RECORD");
-                int id = Integer.parseInt(pieces[0]);//saves the first value of the line which is the ID
-                String action = pieces[1];//saves the second value of a line to decide what to do with current record
-                Cow newcow = new Cow(id);//creates a new cow using the id
-                int value = listcheck(cowlist,id); //NEED TO IMPROVE THIS FIND FUNCTION !!!!!!!!!!!!!!!!!
-                if(value != -1){//if the cow does not already exist in the list
+                int id = Integer.parseInt(pieces[0]);// saves the first value of the line which is the ID
+                String action = pieces[1];// saves the second value of a line to decide what to do with current record
+                Cow newcow = new Cow(id);// creates a new cow using the id
+                int value = listcheck(cowlist, id); // NEED TO IMPROVE THIS FIND FUNCTION !!!!!!!!!!!!!!!!!
+                if (value != -1) {// if the cow does not already exist in the list
                     int amount;
-                    switch(action){
+                    switch (action) {
                         case "W":// cow was wieghed
-                            amount = Integer.parseInt(pieces[2]);//store cow weight amount third value 
-                            cowlist.get(value).setlatest_weight(amount);//set the cow weight, can be one line
+                            amount = Integer.parseInt(pieces[2]);// store cow weight amount third value
+                            cowlist.get(value).setlatest_weight(amount);// set the cow weight, can be one line
                             break;
                         case "M":// cow was Milked
-                            amount = Integer.parseInt(pieces[2]);//store the amount milked , third value in a record
-                            cowlist.get(value).setgal_milk(amount);//set the lates milk recording
+                            amount = Integer.parseInt(pieces[2]);// store the amount milked , third value in a record
+                            cowlist.get(value).setgal_milk(amount);// set the lates milk recording
                             break;
-                        case "T"://cow temp was taken
+                        case "T":// cow temp was taken
                             amount = Integer.parseInt(pieces[2]);// store the temperature
-                            cowlist.get(value).settemp(amount);//set the cow's latest temp
+                            cowlist.get(value).settemp(amount);// set the cow's latest temp
                             break;
                     }
                 }
-            
-                else{
+
+                else {
                     int amount;
-                    switch(action){
+                    switch (action) {
                         case "W":// cow was wieghed
-                            amount = Integer.parseInt(pieces[2]);//store cow weight amount third value 
-                            newcow.setlatest_weight(amount);//set the cow weight, can be one line
+                            amount = Integer.parseInt(pieces[2]);// store cow weight amount third value
+                            newcow.setlatest_weight(amount);// set the cow weight, can be one line
                             break;
                         case "M":// cow was Milked
-                            amount = Integer.parseInt(pieces[2]);//store the amount milked , third value in a record
-                            newcow.setgal_milk(amount);//set the lates milk recording
+                            amount = Integer.parseInt(pieces[2]);// store the amount milked , third value in a record
+                            newcow.setgal_milk(amount);// set the lates milk recording
                             break;
-                        case "T"://cow temp was taken
+                        case "T":// cow temp was taken
                             amount = Integer.parseInt(pieces[2]);// store the temperature
-                            newcow.settemp(amount);//set the cow's latest temp
+                            newcow.settemp(amount);// set the cow's latest temp
                             break;
                     }
-                    cowlist.add(newcow);//add a new cow
+                    cowlist.add(newcow);// add a new cow
                     System.out.println("ADDED");
                 }
-            }
-            else{
+            } else {
                 System.out.println("BAD RECORD");// record does not have the correct amount of values
             }
         }
-        for(int i = 0;i < cowlist.size(); i++){
+        for (int i = 0; i < cowlist.size(); i++) {
             System.out.println(cowlist.get(i));
         }
         System.out.print(cowlist.size());
@@ -82,17 +92,16 @@ public class Practice {
         fr.close();// closes the file reader
     }
 
-    public static int listcheck(ArrayList<Cow> cowlist,  int id){//method to check a list and see if it exists
-        for(int i = 0; i < cowlist.size(); i++){
-            if (cowlist.get(i).getid() == id){
+    public static int listcheck(ArrayList<Cow> cowlist, int id) {// method to check a list and see if it exists
+        for (int i = 0; i < cowlist.size(); i++) {
+            if (cowlist.get(i).getid() == id) {
                 return i;
             }
         }
         return -1;
     }
 
-
-    public boolean inputcheck(String input){
+    public static boolean inputcheck(String input) {
         try{
             Integer.parseInt(input);
         }
